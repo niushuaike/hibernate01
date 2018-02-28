971d40b696cac737a560cb4976b38a87b240489f
nsk-devil <nsjkabc@gmail.com>
2018-02-28 11:57:40 +0800

    2

--- test/TestHibernate.java
+++ test/TestHibernate.java
@@ -4,7 +4,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * 春风再美也比不上你的笑。。。。。
@@ -28,16 +31,62 @@ public class TestHibernate {
         session.close();
     }
 
+    /*
+    *    1、事务的完善写法
+     *   2、验证一级缓存
+     *   3、验证自动修改持久态对象，快照区的存在
+    */
     @Test
-    public void testquery() {
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
+    public void testCache() {
+        SessionFactory sessionFactory=null;
+        Session session=null;
+        Transaction transaction=null;
+        try {
+            sessionFactory = HibernateUtil.getSessionFactory();
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            User user = session.get(User.class, 1);
+            System.out.println(user);
+            user.setName("西方不败");
+            User user1 = session.get(User.class, 1);
+            System.out.println(user1);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (null!=transaction){
+                transaction.rollback();
+            }
+        } finally {
+            if (null!=session){
+                session.close();
+            }
+        }
+    }
 
-        User user = session.get(User.class, 1);
-        System.out.println(user);
-
-        transaction.commit();
-        session.close();
+    @Test
+    public void testQuery() {
+        SessionFactory sessionFactory=null;
+        Session session=null;
+        Transaction transaction=null;
+        try {
+            sessionFactory = HibernateUtil.getSessionFactory();
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            Query<User> query = session.createQuery("from User ", User.class);
+            List<User> list = query.list();
+            for(User user:list){
+                System.out.println(user);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (null!=transaction){
+                transaction.rollback();
+            }
+        } finally {
+            if (null!=session){
+                session.close();
+            }
+        }
     }
 }
